185151cf7f36b76530eca69e368ba87fb681e51e
Jim Denton <inkpen1986@me.com>
2013-01-01 22:12:00 -0700

    corrected spelling error

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Dispatch
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assests.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
