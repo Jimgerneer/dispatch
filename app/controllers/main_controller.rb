94ef5c5c6df95821f3f769b7338515443fcfa598
Jim Denton <inkpen1986@me.com>
2013-01-01 20:45:37 -0700

    Styled front page and forms

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,12 @@
 class MainController < ApplicationController
 
   def index
-    @perps = Perpetrator.leaderboard.all(order: "record_count DESC")
-    @perps_by_bounty = Perpetrator.leaderboard.all(order: "max_bounty DESC")
+    @perps = Perpetrator.leaderboard.all(order: "max_bounty DESC")
+    @most_wanted = @perps.first
+    if params["order"] == 'highest_bounty'
+      @perps = Perpetrator.leaderboard.all(order: "max_bounty DESC")
+    elsif params["order"] == 'most_reports'
+      @perps = Perpetrator.leaderboard.all(order: "record_count DESC")
+    end
   end
 end
