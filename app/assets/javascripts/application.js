ed9b92958ece9a6ac9f0cd35cb74608891526253
Jim Denton <inkpen1986@me.com>
2013-02-05 10:19:04 -0700

    Google tracking added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,14 @@ $(function() {
 window.openRedditWindow = function(url) {
   return $(window.open(url, "Reddit", "status = 1, height = 850, width = 1000, resizable = 0"));
 };
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38261203-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
